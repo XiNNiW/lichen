f3c6d3c58ebc398a16869ec1820ea9d2a74d01b8
David Minnix <dlminnix09@gmail.com>
2020-11-18 10:07:00 -0500

    I am struggling to get the Big 5 working for this class

--- libFungi_interpreter/src/interpreter/data_varient.h
+++ libFungi_interpreter/src/interpreter/data_varient.h
@@ -4,20 +4,25 @@
 #include <vector>
 #include <iostream>
 
-template <typename INTEGER, typename FLOAT, typename STRING, typename SIGNAL>
+// template <typename INTEGER, typename FLOAT, typename STRING, typename SIGNAL>
+
 struct SporeDataVariant {
+    using INTEGER = int;
+    using FLOAT = double;
+    using STRING = std::string;
+    using SIGNAL = std::function<double(int)>;
     using LAMBDA = std::function<SporeDataVariant(SporeDataVariant)>;
     using LIST = std::vector<SporeDataVariant>;
     using MAP = std::map<SporeDataVariant,SporeDataVariant>;
-    enum SPORE_TYPE {t_int, t_float, t_string, t_signal, t_lambda, t_list, t_map} type;  
+    enum SPORE_TYPE {t_int, t_float, t_string, t_signal, t_lambda, t_list, t_map, t_unknown} type;  
     union {
         INTEGER as_integer;
         FLOAT as_float;
         STRING as_string;
-        SIGNAL as_signal;
-        LAMBDA as_lambda;
-        LIST as_list;
-        MAP as_map;
+        // SIGNAL as_signal;
+        // LAMBDA as_lambda;
+        // LIST as_list;
+       // MAP as_map;
     };
     friend void swap(SporeDataVariant& v1, SporeDataVariant& v2){
         std::cout <<"in swap fmethod" << std::endl;
@@ -34,25 +39,27 @@ struct SporeDataVariant {
             case t_string:
                 swap(v1.as_string, v2.as_string);
                 break;
-            case t_signal:
-                swap(v1.as_signal, v2.as_signal);
-                break;
-            case t_lambda:
-                swap(v1.as_lambda, v2.as_lambda);
-                break;
-            case t_list:
-                swap(v1.as_list, v2.as_list);
-                break;
-            case t_map:
-                swap(v1.as_map,v2.as_map);
+            // case t_signal:
+            //     swap(v1.as_signal, v2.as_signal);
+            //     break;
+            // case t_lambda:
+            //     swap(v1.as_lambda, v2.as_lambda);
+            //     break;
+            // case t_list:
+            //     swap(v1.as_list, v2.as_list);
+            //     break;
+            // case t_map:
+            //     swap(v1.as_map,v2.as_map);
+            //     break;
+            case t_unknown:
                 break;
             default:
-                break;
+                assert(false);
         }
     }
-    SporeDataVariant(){
-        type = t_int;
-        as_integer = 0;
+    SporeDataVariant():type(t_unknown){
+        std::cout << "in default cons..."<<std::endl;
+
     }
     //1. DESTRUCTOR
     ~SporeDataVariant() {
@@ -66,126 +73,120 @@ struct SporeDataVariant {
             case t_string:
                 as_string.~STRING();
                 break;
-            case t_signal:
-                as_signal.~SIGNAL();
-                break;
-            case t_lambda:
-                as_lambda.~LAMBDA();
-                break;
-            case t_list:
-                as_list.~LIST();
-                break;
-            case t_map:
-                as_map.~MAP();
+            // case t_signal:
+            //     as_signal.~SIGNAL();
+            //     break;
+            // case t_lambda:
+            //     as_lambda.~LAMBDA();
+            //     break;
+            // case t_list:
+            //     as_list.~LIST();
+            //     break;
+            // case t_map:
+            //     as_map.~MAP();
+            //     break;
+            case t_unknown:
                 break;
             default:
-                break;
+                assert(false);
 
         }
         
     }
     //2. COPY
-    SporeDataVariant(const SporeDataVariant& v){
+    // SporeDataVariant(const SporeDataVariant& v) = delete;
+    SporeDataVariant(SporeDataVariant& v)
+    // :SporeDataVariant()
+    {
         std::cout<<"input value in copy constructor now has: "<<v.as_string<<std::endl;
-
+        // swap(*this, v); 
+        // *this=v;
         type = v.type;
         switch(type){
             case t_int:
-                // as_integer = INTEGER();
                 new (&as_integer) INTEGER(v.as_integer);
                 break;
             case t_float:
-                // as_float = FLOAT();
                 new (&as_float) FLOAT(v.as_float);
                 break;
             case t_string:
                 new (&as_string) STRING(v.as_string);
                 break;
-            case t_signal:
-                new (&as_signal) SIGNAL(v.as_signal);
-                break;
-            case t_lambda:
-                new (&as_lambda) LAMBDA(v.as_lambda);
-                break;
-            case t_list:
-                new (&as_list) LIST(v.as_list);
-                break;
-            case t_map:
-                new (&as_map) MAP(v.as_map);
+            // case t_signal:
+            //     new (&as_signal) SIGNAL(v.as_signal);
+            //     break;
+            // case t_lambda:
+            //     new (&as_lambda) LAMBDA(v.as_lambda);
+            //     break;
+            // case t_list:
+            //     new (&as_list) LIST(v.as_list);
+            //     break;
+            // case t_map:
+            //     new (&as_map) MAP(v.as_map);
+            //     break;
+            case t_unknown:
                 break;
             default: 
-                std::cout<<"OH SHIT"<<std::endl;               
-                break;
+                assert(false);
+              
 
         }
     }
     //3. COPY ASSIGNMENT
-    SporeDataVariant& operator = (const SporeDataVariant v){
+    SporeDataVariant& operator = (SporeDataVariant v){
         std::cout << "in overloaded assignment"<<std::endl;
         swap(*this, v); 
         return *this;
     }
     //4. MOVE
     SporeDataVariant(SporeDataVariant&& v) noexcept
-    :SporeDataVariant()
     {   
-        std::cout << "in move constructor";
-        swap(*this,v);
+        type = std::move(v.type);
+        switch (type)
+        {
+        case t_int:
+            as_integer = v.as_integer;
+            as_integer = 0;
+            break;
+        case t_float:
+            as_float =  v.as_float;
+            as_float =  0;
+            break;
+        case t_string:
+            as_string =  v.as_string;
+            as_string =  nullptr;
+            break;
+        // case t_signal:
+        //     as_signal =  std::move(v.as_signal);
+        //     break;
+        // case t_lambda:
+        //     as_lambda =  std::move(v.as_lambda);
+        //     break;
+        // case t_list:
+        //     as_list =  std::move(v.as_list);
+        //     break;
+        // case t_map:
+        //     as_map =  std::move(v.as_map);
+        //     break;
+        default:
+            break;
+        }
         
     }
-    //5. MOVE ASSIGNMENT
-    SporeDataVariant operator = (const SporeDataVariant&& v){
-        std::cout << "in overloaded move op..."<<std::endl;
-        // new SporeDataVariant(v);
-        type = std::move(v.type);
-        switch(type){
-            case t_int:
-                // as_integer = INTEGER();
-                new (&as_integer) INTEGER(std::move(v.as_integer));
-                break;
-            case t_float:
-                // as_float = FLOAT();
-                new (&as_float) FLOAT(std::move(v.as_float));
-                break;
-            case t_string:
-                // as_string = STRING();
-                std::cout << "in copy constructor"<<std::endl;
-                std::cout << "it is: "<<std::move(v.as_string)<<std::endl;
-                // as_string = STRING(v.as_string);
-                new (&as_string) STRING(v.as_string);
-                std::cout << "value assigned: "<<std::move(as_string)<<std::endl;
-                break;
-            case t_signal:
-                new (&as_signal) SIGNAL(std::move(v.as_signal));
-                break;
-            case t_lambda:
-                new (&as_lambda) LAMBDA(std::move(v.as_lambda));
-                break;
-            case t_list:
-                // LIST temp = LIST(v.as_list.size());
-                // std::copy(v.as_list.begin(),v.as_list.end(),temp);
-                // as_list = SporeDataVariant(temp).as_list;
-                new (&as_list) LIST(std::move(v.as_list));
-                break;
-            case t_map:
-                new (&as_map) MAP(std::move(v.as_map));
-                break;
-            default:                
-                break;
-        }
-
-        return *this;
-    }
-    SporeDataVariant(const INTEGER& value)
+    // //5. MOVE ASSIGNMENT
+    // SporeDataVariant operator = (SporeDataVariant&& v){
+        
+    //     swap(*this,v);
+    //     return *this;
+    // }
+    explicit SporeDataVariant(const INTEGER& value)
     :type(t_int)
-    // {new (&as_integer) INTEGER(value);}
     ,as_integer(value)
     {
-        // new (&as_integer) INTEGER(value);
         std::cout << "making int... " << as_integer << std::endl;
     }
 
-    SporeDataVariant(const FLOAT& value)
+    explicit SporeDataVariant(const FLOAT& value)
     :type(t_float)
     // {new (&as_float) FLOAT(value);}
     ,as_float(value)
@@ -194,89 +195,85 @@ struct SporeDataVariant {
 
     }
     
-    SporeDataVariant(const STRING& value)
+    explicit SporeDataVariant(const STRING& value)
     :type(t_string)
-    // {new (&as_string) STRING(value);
-    //     std::cout<<"object now has: "<<as_string<<std::endl;
-    // }
-    ,as_string(value)
-    // ,as_string(std::move(value))
     {
+        new (&as_string) STRING(value);
         std::cout<<"object now has: "<<as_string<<std::endl;
 
     }
 
-    SporeDataVariant(const SIGNAL& value)
-    :type(t_signal)
-    // {new (&as_signal) SIGNAL(value);}
-    ,as_signal(value)
-    {
-        std::cout << "making signal... " << as_signal(0) << std::endl;
+    // explicit SporeDataVariant(const SIGNAL& value)
+    // :type(t_signal)
+    // // ,as_signal(value)
+    // {
+    //     new (&as_signal) SIGNAL(value);
+    //     std::cout << "making signal... " << as_signal(0) << std::endl;
 
-    }
+    // }
 
-    SporeDataVariant(const LAMBDA& value)
-    :type(t_lambda)
-    // {new (&as_lambda) LAMBDA(value);}
-    ,as_lambda(value)
-    {
-        std::cout << "making lambda..." << std::endl;
-    }
+    // explicit SporeDataVariant(const LAMBDA& value)
+    // :type(t_lambda)
+    // // ,as_lambda(value)
+    // {
+    //     new (&as_lambda) LAMBDA(value);
+    //     std::cout << "making lambda..." << std::endl;
+    // }
 
-    SporeDataVariant(const LIST& value)
-    :type(t_list)
-    // {new (&as_list) LIST(value);}
-    ,as_list(value)
-    {
-        std::cout << "making list..." << std::endl;
+    // explicit SporeDataVariant(const LIST& value)
+    // :type(t_list)
+    // // ,as_list(value)
+    // {
+    //     new (&as_list) LIST(value);
+    //     std::cout << "making list..." << std::endl;
 
-    }
+    // }
 
-    SporeDataVariant(const MAP& value)
-    :type(t_map)
-    // {new (&as_map) MAP(value);}
-    ,as_map(value)
-    {
-        std::cout << "making map..." << std::endl;
+    // explicit SporeDataVariant(const MAP& value)
+    // :type(t_map)
+    // // ,as_map(value)
+    // {
+    //     new (&as_map) MAP(value);
+    //     std::cout << "making map..." << std::endl;
 
-    }
+    // }
 
+    // bool operator == (const SporeDataVariant& v){
+    //     std::cout<< "evaluating equality"<<std::endl;
+    //     bool areEqual = false;
+    //     bool typesMatch = type == v.type;
+    //     if (typesMatch){
+    //         switch (type){
+    //             case t_int:
+    //                 areEqual = as_integer == v.as_integer;
+    //                 break;
+    //             case t_float:
+    //                 areEqual = as_float == v.as_float;
+    //                 break;
+    //             case t_string:
+    //                 areEqual = as_string == v.as_string;
+    //                 break;
+    //             case t_signal:
+    //                 areEqual = as_signal == v.as_signal;
+    //                 break;
+    //             case t_lambda:
+    //                 areEqual = as_lambda == v.as_lambda;
+    //                 break;
+    //             case t_list:
+    //                 areEqual = as_list == v.as_list;
+    //                 break;
+    //             case t_map:
+    //                 areEqual = as_map == v.as_map;
+    //                 break;
+    //             case t_unknown:
+    //                 break;
+    //             default:
+    //                 assert(false);
 
+    //         }
+    //     } 
 
-    bool operator == (const SporeDataVariant& v){
-        std::cout<< "evaluating equality"<<std::endl;
-        bool areEqual = false;
-        bool typesMatch = type == v.type;
-        if (typesMatch){
-            switch (type){
-                case t_int:
-                    areEqual = as_integer == v.as_integer;
-                    break;
-                case t_float:
-                    areEqual = as_float == v.as_float;
-                    break;
-                case t_string:
-                    areEqual = as_string == v.as_string;
-                    break;
-                case t_signal:
-                    areEqual = as_signal == v.as_signal;
-                    break;
-                case t_lambda:
-                    areEqual = as_lambda == v.as_lambda;
-                    break;
-                case t_list:
-                    areEqual = as_list == v.as_list;
-                    break;
-                case t_map:
-                    areEqual = as_map == v.as_map;
-                    break;
-                
-                default:
-                    break;
-            }
-        } 
-
-        return areEqual;
-    }
+    //     return areEqual;
+    // }
 
 };
--- libFungi_interpreter/test/varient_test.cpp
+++ libFungi_interpreter/test/varient_test.cpp
@@ -7,50 +7,51 @@
 #include "../src/interpreter/data_varient.h"
 #include <iostream>
 
-typedef SporeDataVariant<int,double,std::string,std::function<double(int)>> SPV;
+typedef SporeDataVariant SPV;
+// typedef SporeDataVariant<int,double,std::string,std::function<double(int)>> SPV;
 
-TEST(VarientTest, IsTaggedUnionOfTemplateArgs) {
+// TEST(VarientTest, IsTaggedUnionOfTemplateArgs) {
 
-    SPV* v_i = new SPV(5);
-    ASSERT_EQ(SPV::t_int,v_i->type);
-    ASSERT_EQ(5,v_i->as_integer);
+//     SPV* v_i = new SPV(5);
+//     ASSERT_EQ(SPV::t_int,v_i->type);
+//     ASSERT_EQ(5,v_i->as_integer);
 
-    SPV* v_d = new SPV(5.3);
-    ASSERT_EQ(SPV::t_float,v_d->type);
-    ASSERT_EQ(5.3,v_d->as_float);
+//     SPV* v_d = new SPV(5.3);
+//     ASSERT_EQ(SPV::t_float,v_d->type);
+//     ASSERT_EQ(5.3,v_d->as_float);
 
-    SPV* v_str = new SPV("hello");
-    ASSERT_EQ(SPV::t_string,v_str->type);
-    ASSERT_EQ("hello",v_str->as_string);
-
-
-    SPV* v_sig 
-        = new SPV(
-            std::function<double(int)>(
-                [](int t){return 1.22;}
-            )
-        );
+//     SPV* v_str = new SPV("hello");
+//     ASSERT_EQ(SPV::t_string,v_str->type);
+//     ASSERT_EQ("hello",v_str->as_string);
 
 
-    
-    SPV* v_lam
-        = new SPV(
-            std::function<SPV(SPV)>(
-                [](SPV arg){return SPV(3);}
-            )
-        );
+//     SPV* v_sig 
+//         = new SPV(
+//             std::function<double(int)>(
+//                 [](int t){return 1.22;}
+//             )
+//         );
+
 
     
-    // std::vector<SPV> expected_vector = std::vector<SPV>({SPV(9), SPV(9)});
-    SPV item1 = SPV("goober");
+//     SPV* v_lam
+//         = new SPV(
+//             std::function<SPV(SPV)>(
+//                 [](SPV arg){return SPV(3);}
+//             )
+//         );
 
-    SPV item2 = SPV(9);
+    
+//     // std::vector<SPV> expected_vector = std::vector<SPV>({SPV(9), SPV(9)});
+//     SPV item1 = SPV("goober");
 
-    SPV item3 = item1;
-    std::cout << "made it halfway"<<std::endl;
+//     SPV item2 = SPV(9);
 
-    // std::vector<SPV> expected_vector = std::vector<SPV>({item1, item2});
-    // SPV* v_list = new SPV(expected_vector);
+//     SPV item3 = item1;
+//     std::cout << "made it halfway"<<std::endl;
+
+//     // std::vector<SPV> expected_vector = std::vector<SPV>({item1, item2});
+//     // SPV* v_list = new SPV(expected_vector);
 
 
 
@@ -58,75 +59,86 @@ TEST(VarientTest, IsTaggedUnionOfTemplateArgs) {
 
    
 
-    ASSERT_EQ(SPV::t_signal,v_sig->type);
-    auto the_signal = v_sig->as_signal;
-    ASSERT_EQ(1.22,the_signal(0));
+//     ASSERT_EQ(SPV::t_signal,v_sig->type);
+//     auto the_signal = v_sig->as_signal;
+//     ASSERT_EQ(1.22,the_signal(0));
 
-    // ASSERT_EQ(SPV::t_list, v_list->type);
-    // std::vector<SPV> actual_list = v_list->as_list;
-    // ASSERT_EQ(2,actual_list.size());
-    // ASSERT_EQ(SPV::t_string,actual_list[0].type);
-    // ASSERT_EQ("goober",actual_list[0].as_string);
-    // ASSERT_EQ(SPV::t_int,actual_list[1].type);
-    // ASSERT_EQ(9,actual_list[1].as_integer);
+//     // ASSERT_EQ(SPV::t_list, v_list->type);
+//     // std::vector<SPV> actual_list = v_list->as_list;
+//     // ASSERT_EQ(2,actual_list.size());
+//     // ASSERT_EQ(SPV::t_string,actual_list[0].type);
+//     // ASSERT_EQ("goober",actual_list[0].as_string);
+//     // ASSERT_EQ(SPV::t_int,actual_list[1].type);
+//     // ASSERT_EQ(9,actual_list[1].as_integer);
 
 
+// }
+
+TEST(VarientTest, Properties){
+    EXPECT_TRUE(std::is_move_constructible<SPV>::value);
+    EXPECT_TRUE(std::is_move_assignable<SPV>::value);
+    EXPECT_TRUE(std::is_nothrow_move_assignable<SPV>::value);
+
+    EXPECT_TRUE(std::is_copy_assignable<SPV>::value);
+    EXPECT_TRUE(std::is_copy_constructible<SPV>::value);
+    EXPECT_TRUE(std::is_default_constructible<SPV>::value);
+    EXPECT_TRUE(std::is_destructible<SPV>::value);
 }
 
-TEST(VarientTest, CopyConstructor){
-    std::cout << "in copy cons test" <<std::endl;
-    SPV int1 = SPV(1);
-    std::cout << "made int: "<< int1.as_integer <<std::endl;
+// TEST(VarientTest, CopyConstructor){
+//     std::vector<SPV> foo({SPV(0)});
+//     std::cout << "in copy cons test" <<std::endl;
+//     SPV int1 = SPV(1);
+//     std::cout << "made int: "<< int1.as_integer <<std::endl;
 
-    // SPV int2 = SPV(int1);
-    std::cout << "(outer )made int: "<< int1.as_integer <<std::endl;
+//     std::cout << "(outer )made int: "<< int1.as_integer <<std::endl;
 
-    SPV int2 = int1;
-    // std::cout << "copied int: " << int2->as_integer<< std::endl;
+//     SPV int2 = SPV(int1);
+//     // std::cout << "copied int: " << int2->as_integer<< std::endl;
 
 
-    ASSERT_EQ(SPV::t_int,int1.type);
-    // ASSERT_EQ(SPV::t_int,int2.type);
-    // ASSERT_EQ(int1.as_integer,int2.as_integer);
+//     ASSERT_EQ(SPV::t_int,int1.type);
+//     // ASSERT_EQ(SPV::t_int,int2.type);
+//     // ASSERT_EQ(int1.as_integer,int2.as_integer);
 
-    std::cout<< "about to make float"<<std::endl;
-    SPV float1 = SPV(1.22);
-    std::cout << "made float" <<std::endl;
+//     std::cout<< "about to make float"<<std::endl;
+//     SPV float1 = SPV(1.22);
+//     std::cout << "made float" <<std::endl;
 
-    // SPV float2 = float1;
-    // std::cout << "copied float" <<std::endl;
+//     // SPV float2 = float1;
+//     // std::cout << "copied float" <<std::endl;
 
-    // ASSERT_EQ(SPV::t_float,float1.type);
-    // ASSERT_EQ(SPV::t_float,float2.type);
-    // ASSERT_EQ(float1.as_float,float2.as_float);
+//     // ASSERT_EQ(SPV::t_float,float1.type);
+//     // ASSERT_EQ(SPV::t_float,float2.type);
+//     // ASSERT_EQ(float1.as_float,float2.as_float);
 
-    // SPV string1 = SPV("goober");
-    // SPV string2 = string1;
-    // ASSERT_EQ(SPV::t_string,string1.type);
-    // ASSERT_EQ(SPV::t_string,string2.type);
-    // ASSERT_EQ(string1.as_string,string2.as_string);
+//     // SPV string1 = SPV("goober");
+//     // SPV string2 = string1;
+//     // ASSERT_EQ(SPV::t_string,string1.type);
+//     // ASSERT_EQ(SPV::t_string,string2.type);
+//     // ASSERT_EQ(string1.as_string,string2.as_string);
 
-    // SPV signal1 = std::function<double(int)>([](int t){return 5.4;});
-    // SPV signal2 = signal1;
-    // ASSERT_EQ(SPV::t_signal,signal1.type);
-    // ASSERT_EQ(SPV::t_signal,signal2.type);
-    // ASSERT_EQ(signal1.as_signal(0),signal2.as_signal(0));
+//     // SPV signal1 = std::function<double(int)>([](int t){return 5.4;});
+//     // SPV signal2 = signal1;
+//     // ASSERT_EQ(SPV::t_signal,signal1.type);
+//     // ASSERT_EQ(SPV::t_signal,signal2.type);
+//     // ASSERT_EQ(signal1.as_signal(0),signal2.as_signal(0));
 
-    // SPV list1 = SPV(std::vector<SPV>({int1,string1,signal1}));
-    // SPV list2 = list1;
-    // ASSERT_EQ(SPV::t_list,list1.type);
-    // ASSERT_EQ(SPV::t_list,list2.type);
-    // ASSERT_EQ(list1.as_list.size(),list2.as_list.size());
-    // ASSERT_EQ(3, list1.as_list.size());
-    // ASSERT_EQ(list1.as_list[0].as_integer,list2.as_list[0].as_integer);
-    // ASSERT_EQ(list1.as_list[0].as_string,list2.as_list[0].as_string);
-    // ASSERT_EQ(list1.as_list[0].as_signal(0),list2.as_list[0].as_signal(0));
-    // SPV map1 = SPV(std::map<SPV, SPV>({string1,float2}));
-    // SPV map2 = map1;
-    // ASSERT_EQ(SPV::t_map, map1.type);
-    // ASSERT_EQ(SPV::t_map, map2.type);
-    // ASSERT_EQ(map1.as_map.size(), map2.as_map.size());
-    // ASSERT_EQ(map1.as_map[string1].as_float, map2.as_map[string1].as_float);
+//     // SPV list1 = SPV(std::vector<SPV>({int1,string1,signal1}));
+//     // SPV list2 = list1;
+//     // ASSERT_EQ(SPV::t_list,list1.type);
+//     // ASSERT_EQ(SPV::t_list,list2.type);
+//     // ASSERT_EQ(list1.as_list.size(),list2.as_list.size());
+//     // ASSERT_EQ(3, list1.as_list.size());
+//     // ASSERT_EQ(list1.as_list[0].as_integer,list2.as_list[0].as_integer);
+//     // ASSERT_EQ(list1.as_list[0].as_string,list2.as_list[0].as_string);
+//     // ASSERT_EQ(list1.as_list[0].as_signal(0),list2.as_list[0].as_signal(0));
+//     // SPV map1 = SPV(std::map<SPV, SPV>({string1,float2}));
+//     // SPV map2 = map1;
+//     // ASSERT_EQ(SPV::t_map, map1.type);
+//     // ASSERT_EQ(SPV::t_map, map2.type);
+//     // ASSERT_EQ(map1.as_map.size(), map2.as_map.size());
+//     // ASSERT_EQ(map1.as_map[string1].as_float, map2.as_map[string1].as_float);
 
-}
+// }
 
