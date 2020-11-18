4d1508253a55a390b97cedf95afa821876029785
David Minnix <dlminnix09@gmail.com>
2020-11-18 14:21:49 -0500

    now we have some kind of basic varient... still would like it to store a map as well but the requirements are harder to meet

--- libFungi_interpreter/src/interpreter/data_varient.h
+++ libFungi_interpreter/src/interpreter/data_varient.h
@@ -1,279 +1,222 @@
 #pragma once
 #include <string>
-#include <map>
+#include <unordered_map>
 #include <vector>
+#include <string>
+#include <functional>
 #include <iostream>
+#include "signal_wrapper.h"
 
-// template <typename INTEGER, typename FLOAT, typename STRING, typename SIGNAL>
 
-struct SporeDataVariant {
-    using INTEGER = int;
-    using FLOAT = double;
-    using STRING = std::string;
-    using SIGNAL = std::function<double(int)>;
-    using LAMBDA = std::function<SporeDataVariant(SporeDataVariant)>;
-    using LIST = std::vector<SporeDataVariant>;
-    using MAP = std::map<SporeDataVariant,SporeDataVariant>;
-    enum SPORE_TYPE {t_int, t_float, t_string, t_signal, t_lambda, t_list, t_map, t_unknown} type;  
-    union {
-        INTEGER as_integer;
-        FLOAT as_float;
-        STRING as_string;
-        // SIGNAL as_signal;
-        // LAMBDA as_lambda;
-        // LIST as_list;
-       // MAP as_map;
-    };
-    friend void swap(SporeDataVariant& v1, SporeDataVariant& v2){
-        std::cout <<"in swap fmethod" << std::endl;
-        using std::swap;
 
-        swap(v1.type,v2.type);
-        switch(v1.type){
-            case t_int:
-                swap(v1.as_integer,v2.as_integer);
-                break;
-            case t_float:
-                swap(v1.as_float, v2.as_float);
-                break;
-            case t_string:
-                swap(v1.as_string, v2.as_string);
-                break;
-            // case t_signal:
-            //     swap(v1.as_signal, v2.as_signal);
-            //     break;
-            // case t_lambda:
-            //     swap(v1.as_lambda, v2.as_lambda);
-            //     break;
-            // case t_list:
-            //     swap(v1.as_list, v2.as_list);
-            //     break;
-            // case t_map:
-            //     swap(v1.as_map,v2.as_map);
-            //     break;
-            case t_unknown:
-                break;
-            default:
-                assert(false);
-        }
-    }
-    SporeDataVariant():type(t_unknown){
-        std::cout << "in default cons..."<<std::endl;
-
-    }
-    //1. DESTRUCTOR
-    ~SporeDataVariant() {
-        switch (type){
-            case t_int:
-                as_integer.~INTEGER();
-                break;
-            case t_float:
-                as_float.~FLOAT();
-                break;
-            case t_string:
-                as_string.~STRING();
-                break;
-            // case t_signal:
-            //     as_signal.~SIGNAL();
-            //     break;
-            // case t_lambda:
-            //     as_lambda.~LAMBDA();
-            //     break;
-            // case t_list:
-            //     as_list.~LIST();
-            //     break;
-            // case t_map:
-            //     as_map.~MAP();
-            //     break;
-            case t_unknown:
-                break;
-            default:
-                assert(false);
-
-        }
-        
-    }
-    //2. COPY
-    // SporeDataVariant(const SporeDataVariant& v) = delete;
-    SporeDataVariant(SporeDataVariant& v) noexcept
-    // :SporeDataVariant()
-    {
-        std::cout<<"input value in copy constructor now has: "<<v.as_string<<std::endl;
-        // swap(*this, v); 
-        // *this=v;
-        type = v.type;
-        switch(type){
-            case t_int:
-                new (&as_integer) INTEGER(v.as_integer);
-                break;
-            case t_float:
-                new (&as_float) FLOAT(v.as_float);
-                break;
-            case t_string:
-                new (&as_string) STRING(v.as_string);
-                break;
-            // case t_signal:
-            //     new (&as_signal) SIGNAL(v.as_signal);
-            //     break;
-            // case t_lambda:
-            //     new (&as_lambda) LAMBDA(v.as_lambda);
-            //     break;
-            // case t_list:
-            //     new (&as_list) LIST(v.as_list);
-            //     break;
-            // case t_map:
-            //     new (&as_map) MAP(v.as_map);
-            //     break;
-            case t_unknown:
-                break;
-            default: 
-                assert(false);
-              
-
-        }
-    }
-    //3. COPY ASSIGNMENT
-    SporeDataVariant& operator = (SporeDataVariant v){
-        std::cout << "in overloaded assignment"<<std::endl;
-        swap(*this, v); 
-        return *this;
-    }
-    //4. MOVE
-    SporeDataVariant(SporeDataVariant&& v) noexcept
-    {   
-        type = std::move(v.type);
-        switch (type)
+struct FakeHash{
+    template<typename A>
+    size_t operator()(const A& f){
+        size_t res = 17;
+        res *= 31+std::hash<int>()(f.type);
+        switch (f.type)
         {
-        case t_int:
-            as_integer = v.as_integer;
-            as_integer = 0;
+        case A::t_int:
+            res *= 31+std::hash<int>()(f.as_integer);
             break;
-        case t_float:
-            as_float =  v.as_float;
-            as_float =  0;
+        case A::t_float:
+            res *= 31+std::hash<float>()(f.as_float);
             break;
-        case t_string:
-            as_string =  v.as_string;
-            as_string =  nullptr;
+        case A::t_bool:
+            res *= 31+std::hash<bool>()(f.as_bool);
             break;
-        // case t_signal:
-        //     as_signal =  std::move(v.as_signal);
+        case A::t_string:
+            res *= 31+std::hash<std::string>()(f.as_string);
+            break;
+        case A::t_list:
+            res *= 31+std::hash<std::vector<A>>()(f.as_list);
+            break;
+        // case A::t_map:
+        //     res *= 31+std::hash<std::unordered_map<A,A,FakeHash>>()(f.as_map);
         //     break;
-        // case t_lambda:
-        //     as_lambda =  std::move(v.as_lambda);
-        //     break;
-        // case t_list:
-        //     as_list =  std::move(v.as_list);
-        //     break;
-        // case t_map:
-        //     as_map =  std::move(v.as_map);
-        //     break;
+        
         default:
             break;
         }
-        
+        return res;
     }
-    // //5. MOVE ASSIGNMENT
-    // SporeDataVariant operator = (SporeDataVariant&& v){
-        
-    //     swap(*this,v);
-    //     return *this;
-    // }
-    explicit SporeDataVariant(const INTEGER& value)
-    :type(t_int)
-    ,as_integer(value)
-    {
-        std::cout << "making int... " << as_integer << std::endl;
+};
+
+struct SporeDataVariant{
+    enum {t_int, t_float, t_bool, t_string, t_list, t_map, t_signal, t_lambda, t_unknown} type;
+    union{
+        int as_integer;
+        double as_float;
+        bool as_bool;
+        std::string as_string;
+        std::vector<SporeDataVariant> as_list;
+        Signal<double,int> as_signal;
+        // std::unordered_map<SporeDataVariant,SporeDataVariant,FakeHash> as_map;
+        std::function<SporeDataVariant(SporeDataVariant)> as_lambda;
+    };
+    ~SporeDataVariant(){
+        if(type==t_string){
+            using std::string;
+            as_string.~string();
+        }
+        if(type == t_list){
+            using std::vector;
+            as_list.~vector<SporeDataVariant>();
+        }
+        // if(type== t_map){
+        //     using std::unordered_map;
+        //     as_map.~unordered_map<SporeDataVariant,SporeDataVariant,FakeHash>();
+        // }
+        if(type == t_lambda){
+            using std::function;
+            as_lambda.~function<SporeDataVariant(SporeDataVariant)>();
+        }
+        if(type == t_signal){
+            as_signal.~Signal<double,int>();
+        }
+    }
+    SporeDataVariant():type(t_unknown){}
+    SporeDataVariant(const SporeDataVariant& f)
+    :SporeDataVariant(){
+        type = f.type;
+        switch (type)
+        {
+        case t_int:
+            as_integer=f.as_integer;
+            break;
+         case t_float:
+            as_float=f.as_float;
+            break;
+        case t_bool:
+            as_bool=f.as_bool;
+            break;
+        case t_string:
+            new (&as_string) std::string(f.as_string);
+            break;
+        case t_list:
+            new (&as_list) std::vector<SporeDataVariant>();
+            for(auto el:f.as_list){as_list.push_back(el);}
+            break;
+        // case t_map:
+        //     new (&as_map) std::unordered_map<SporeDataVariant,SporeDataVariant,FakeHash>();
+        //     for(const auto& el: f.as_map){
+        //         as_map.insert(el);
+        //     }
+        //     break;
+        case t_lambda:
+            new (&as_lambda) std::function<SporeDataVariant(SporeDataVariant)>(f.as_lambda);
+            break;
+        case t_signal:
+            new (&as_signal) Signal<double,int>(f.as_signal);
+            break;
+        default:
+            break;
+        }
+    }
+    SporeDataVariant& operator =(SporeDataVariant f) noexcept{
+        swapUnion(*this,f);
+        return *this;
     }
 
-    explicit SporeDataVariant(const FLOAT& value)
-    :type(t_float)
-    // {new (&as_float) FLOAT(value);}
-    ,as_float(value)
-    {
-        std::cout << "making float... " << as_float << std::endl;
+    SporeDataVariant(int v):type(t_int),as_integer(v){std::cout<<"int\n";}
+    SporeDataVariant(double v):type(t_float),as_float(v){std::cout<<"float\n";}
+    SporeDataVariant(bool v):type(t_bool),as_bool(v){std::cout<<"bool\n";}
+    SporeDataVariant(std::string v)
+    :type(t_string){
+        new (&as_string)std::string(v);
+        std::cout<<"string\n";
+    }
+    SporeDataVariant(const char v[] )
+    :type(t_string){
+        new (&as_string) std::string(v);
+        std::cout<<"char*\n";
+    }
+    SporeDataVariant(const std::vector<SporeDataVariant> v)
+    :type(t_list){
+        new (&as_list) std::vector<SporeDataVariant>();
+        for(auto el:v){
+            as_list.push_back(el);
+        }
+        std::cout<<"vector\n";
+    }
+    SporeDataVariant(const Signal<double,int>& v):type(t_signal){
+        new (&as_signal) Signal<double,int>(v);
+        std::cout<<"signal\n";
+
+    }
+    SporeDataVariant(const std::function<SporeDataVariant(SporeDataVariant)>& v)
+    :type(t_lambda){
+        new (&as_lambda) std::function<SporeDataVariant(SporeDataVariant)>(v);
+        std::cout<<"lambda\n";
+    }
+
+    bool operator == (const SporeDataVariant& f) const {
+        bool areEqual = false;
+        areEqual &= type==f.type;
+        if(areEqual){
+            switch (type)
+            {
+            case t_int:
+                areEqual&= as_integer == f.as_integer;
+                break;
+            case t_float:
+                areEqual&= as_float == f.as_float;
+                break;
+            case t_bool:
+                areEqual&= as_bool == f.as_bool;
+                break;
+            case t_string:
+                areEqual&= as_string == f.as_string;
+                break;
+            case t_list:
+                areEqual&= as_list == f.as_list;
+                break;
+            // case t_map:
+            //     areEqual&= as_map == f.as_map;
+            //     break;
+            case t_lambda:
+                areEqual= false;
+                break;
+            case t_signal:
+                areEqual = false;
+            
+            default:
+                break;
+            }
+        }
+        return areEqual;
 
     }
     
-    explicit SporeDataVariant(const STRING& value)
-    :type(t_string)
-    {
-        new (&as_string) STRING(value);
-        std::cout<<"object now has: "<<as_string<<std::endl;
 
+    friend void swapUnion(SporeDataVariant& f1, SporeDataVariant& f2) noexcept{
+        std::cout<<"swap called\n";
+        using std::swap;
+        swap(f1.type,f2.type);
+        switch (f1.type)
+        {
+        case t_int:
+            swap(f1.as_integer, f2.as_integer);
+            break;
+        case t_float:
+            swap(f1.as_float, f2.as_float);
+            break;
+        case t_bool:
+            swap(f1.as_bool, f2.as_bool);
+            break;
+        case t_string:
+            swap(f1.as_string, f1.as_string);
+        case t_list:
+            swap(f1.as_list, f1.as_list);
+        case t_lambda:
+            swap(f1.as_lambda, f1.as_lambda);
+        // case t_map:
+        //     swap(f1.as_map, f2.as_map);
+        case t_signal:
+            swap(f1.as_signal,f2.as_signal);
+        default:
+            break;
+        }
     }
-
-    // explicit SporeDataVariant(const SIGNAL& value)
-    // :type(t_signal)
-    // // ,as_signal(value)
-    // {
-    //     new (&as_signal) SIGNAL(value);
-    //     std::cout << "making signal... " << as_signal(0) << std::endl;
-
-    // }
-
-    // explicit SporeDataVariant(const LAMBDA& value)
-    // :type(t_lambda)
-    // // ,as_lambda(value)
-    // {
-    //     new (&as_lambda) LAMBDA(value);
-    //     std::cout << "making lambda..." << std::endl;
-    // }
-
-    // explicit SporeDataVariant(const LIST& value)
-    // :type(t_list)
-    // // ,as_list(value)
-    // {
-    //     new (&as_list) LIST(value);
-    //     std::cout << "making list..." << std::endl;
-
-    // }
-
-    // explicit SporeDataVariant(const MAP& value)
-    // :type(t_map)
-    // // ,as_map(value)
-    // {
-    //     new (&as_map) MAP(value);
-    //     std::cout << "making map..." << std::endl;
-
-    // }
-
-    // bool operator == (const SporeDataVariant& v){
-    //     std::cout<< "evaluating equality"<<std::endl;
-    //     bool areEqual = false;
-    //     bool typesMatch = type == v.type;
-    //     if (typesMatch){
-    //         switch (type){
-    //             case t_int:
-    //                 areEqual = as_integer == v.as_integer;
-    //                 break;
-    //             case t_float:
-    //                 areEqual = as_float == v.as_float;
-    //                 break;
-    //             case t_string:
-    //                 areEqual = as_string == v.as_string;
-    //                 break;
-    //             case t_signal:
-    //                 areEqual = as_signal == v.as_signal;
-    //                 break;
-    //             case t_lambda:
-    //                 areEqual = as_lambda == v.as_lambda;
-    //                 break;
-    //             case t_list:
-    //                 areEqual = as_list == v.as_list;
-    //                 break;
-    //             case t_map:
-    //                 areEqual = as_map == v.as_map;
-    //                 break;
-    //             case t_unknown:
-    //                 break;
-    //             default:
-    //                 assert(false);
-
-    //         }
-    //     } 
-
-    //     return areEqual;
-    // }
 
 };
---
+++ libFungi_interpreter/src/interpreter/signal_wrapper.h
@@ -0,0 +1,43 @@
+#pragma once
+#include <functional>
+
+template<typename Sample,typename Time>
+struct Signal{
+    std::function<Sample(Time)> _sig;
+    ~Signal(){
+        using std::function;
+        _sig.~function<Sample(Time)>();
+    }
+    Signal(const Signal& s):_sig(s._sig){}
+    Signal(std::function<Sample(Time)> f):_sig(f){}
+    friend Signal add(Signal a, Signal b){
+        return Signal(std::function<Sample(Time)>([=](Time t){return a(t)+b(t);}));
+    }
+    Signal operator +(Signal a){
+        return add(*this,a);
+    }
+    friend Signal subtract(Signal a, Signal b){
+        return Signal(std::function<Sample(Time)>([=](Time t){return a(t)-b(t);}));
+    }
+    Signal operator -(Signal a){
+        return subtract(*this,a);
+    }
+    friend Signal mult(Signal a, Signal b){
+        return Signal(std::function<Sample(Time)>([=](Time t){return a(t)*b(t);}));
+    }
+    Signal operator *(Signal a){
+        return mult(*this,a);
+    }
+    friend Signal divide(Signal a, Signal b){
+        return Signal(std::function<Sample(Time)>([=](Time t){return a(t)/b(t);}));
+    }
+    Signal operator /(Signal a){
+        return divide(*this,a);
+    }
+
+    Sample operator()(Time t){
+        return _sig(t);
+    }
+
+
+};
