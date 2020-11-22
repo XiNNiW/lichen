bd662937d3cdcbca1e8408ccb2281c0bfefd74fe
David Minnix <dlminnix09@gmail.com>
2020-11-22 17:35:07 -0500

    working through first very basic interpreter operations

--- libFungi_interpreter/test/interpreter_test.cpp
+++ libFungi_interpreter/test/interpreter_test.cpp
@@ -2,6 +2,34 @@
 #include "../src/interpreter/interpreter.h"
 #include "../src/lang/sporelang_syntaxtree.h"
 #include "../src/interpreter/data_varient.h"
+#include <iostream>
+using std::cout;
+
+TEST(InterpreterTest, Assignment_Expression) {
+
+    FungiInterpreter* interpreter = new FungiInterpreter();
+    cout<<"made interpreter\n";
+    AssignmentNode* instructions 
+        = new AssignmentNode(
+            new IdentifierNode("foo"),
+            new AddOperatorNode(
+                new IntegerNode(5),
+                new IntegerNode(3)
+            )
+        );
+    cout<<"made AST\n";
+    
+    interpreter->inspect(instructions);
+    cout<<"inspected tree\n";
+
+    Either<RuntimeError, SporeDataVariant> errorOrResult = interpreter->curretResult;
+    ASSERT_TRUE(errorOrResult.isRight());
+    SporeDataVariant resultSymbol = errorOrResult.getRight();
+    ASSERT_EQ(SporeDataVariant::t_unknown, resultSymbol.type);
+
+    // ASSERT_EQ(SporeDataVariant::t_data_variable, interpreter->globalScope.lookupInCurrentScopeOnly("foo").type)
+
+}
 
 TEST(InterpreterTest, BinaryOp_Multiply) {
     FungiInterpreter* interpreter = new FungiInterpreter();
